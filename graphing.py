a0de4f394c58a9683bf60d5a1cda9b328ce055ab
RajVhora <rajvhora2000@outlook.com>
2022-09-24 02:49:59 +0530

    Added filtering and edited comments

--- graphing.py
+++ graphing.py
@@ -2,11 +2,15 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 #Sequence of Data Headers
-#X/30.0,Y,sig11/4.0,sig22/4.0,sig12/4.0
+#X,Y,sig11,sig22,sig12
 
 print("Reading Data from analytical.dat")
 #Import .dat file and plot data
 data = np.loadtxt('analytical.dat')
+
+#Filter
+data = data[data[:,0] < 8]
+
 plt.plot(data[:,0],data[:,2],label='sig11')
 plt.plot(data[:,0],data[:,3],label='sig22')
 plt.plot(data[:,0],data[:,4],label='sig12')
